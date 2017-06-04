9fce4ad1e8112beaae9539af1a0d5c1820f13586
john <20131256@ce.pucmm.edu.do>
2017-06-04 17:34:03 -0400

    Created classes Comentario and Etiqueta

---
+++ src/main/java/com/modelo/Etiqueta.java
@@ -0,0 +1,34 @@
+package com.modelo;
+
+/**
+ * Created by john on 04/06/17.
+ */
+public class Etiqueta {
+    private long id;
+    private String etiqueta;
+
+    public Etiqueta(long id, String etiqueta) {
+        this.id = id;
+        this.etiqueta = etiqueta;
+    }
+
+    public Etiqueta() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEtiqueta() {
+        return etiqueta;
+    }
+
+    public void setEtiqueta(String etiqueta) {
+        this.etiqueta = etiqueta;
+    }
+
+}
